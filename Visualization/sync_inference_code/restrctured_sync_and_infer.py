684ac4fb15969e0c526ab822cc389bd41cbc947f
Girgis98 <girgis.mamdouh@gmail.com>
2024-02-19 02:08:02 +0000

    modified inference as oop code, modifed sync and infer script to cope with this

--- Visualization/sync_inference_code/restrctured_sync_and_infer.py
+++ Visualization/sync_inference_code/restrctured_sync_and_infer.py
@@ -4,14 +4,13 @@ import time
 import traceback
 
 sys.path.append(os.path.abspath(os.path.join(__file__, "../../../")))
-from CancerDetection.scripts.infer import infer as perform_segmentation
+from CancerDetection.scripts.infer_restructure import CancerSegmentation
 from params import poll_interval_seconds
 from utils.utils import get_models_dict, get_models_paths, save_new_patients, list_new_patients
+from time import time as ctime
 
-model = None
-
-def process_new_patients():
-    global model
+def process_new_patients(cancer_segmentation):
+    st_tot = ctime()
     for patient in list_new_patients():
         try:
             """
@@ -52,6 +51,7 @@ def process_new_patients():
                             if not then get intersection , compare with fourth if exceeds use three series pair,
                             if not intersect and use all four
             """
+            st = ctime()
             infer = patient.validate()
             if not infer:
                 patient.delete_temp()
@@ -65,8 +65,8 @@ def process_new_patients():
             models_paths = get_models_paths(inference_modes, models_dict)
             segmentation_results = []
             for model_path in models_paths:
-                segmentation_result, model = perform_segmentation(
-                    model_path[1], patient.data_dir_path, model_path[0], model=model
+                segmentation_result = cancer_segmentation.infer(
+                    model_path[1], patient.data_dir_path, model_path[0]
                 ) # model_path, infer_example_dir, inference_modes (used for this model)
                 segmentation_results.append((model_path[0], segmentation_result))
             patient.set_segmentation_result(segmentation_results)
@@ -74,17 +74,22 @@ def process_new_patients():
             patient.upload_dicom_seg()
             save_new_patients([patient])
             patient.delete_temp()
+            print(f"Time took to process patient: {patient.id} is: {ctime()-st}")
         except Exception as e:
             print(f'An error occurred: {e}')
             traceback.print_exc()
             continue
+    print(f"Time took to process all patients: is: {ctime()-st_tot}")
 
 
 def main():
     # This script could be scheduled to run at regular intervals
+    st = ctime()
+    cancer_segmentation = CancerSegmentation()
+    print(f"Time to initialize model with GPU is {ctime()-st}")
     while True:
         try:
-            process_new_patients()
+            process_new_patients(cancer_segmentation)
             print("Waiting...")
             time.sleep(poll_interval_seconds)
         except Exception as e:
