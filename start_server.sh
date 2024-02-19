c8463f4d43c660ebd0de000c028455291a79eae3
Girgis98 <girgis.mamdouh@gmail.com>
2024-02-19 02:08:39 +0000

    switched calls order

--- start_server.sh
+++ start_server.sh
@@ -17,19 +17,19 @@ echo "All Docker containers have been stopped and deleted, ready to restart serv
 # Change directory to the VISUALIZER_DIR_PATH
 cd "$VISUALIZER_DIR_PATH"
 
-# Start the orthanc service in the background and detach it
-nohup yarn orthanc:up > orthanc_up.log 2>&1 &
-
-echo "Starting orthanc:up, waiting for $timeout seconds to initialize..."
-# Wait for a bit
-sleep $timeout
-
 # Start viewer after launching orthanc server
 echo "Starting dev:orthanc..."
 yarn run dev:orthanc --verbose > orthanc_run.log &
 
 echo "Waiting for $timeout seconds to allow dev:orthanc to initialize properly..."
 # Wait for another a bit
+sleep $timeout
+
+# Start the orthanc service in the background and detach it
+nohup yarn orthanc:up > orthanc_up.log 2>&1 &
+
+echo "Starting orthanc:up, waiting for $timeout seconds to initialize..."
+# Wait for a bit
 sleep $timeout
 
 # Execute the sync and infer script
